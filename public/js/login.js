595c515a519d17969695613fed907ae8d6a60ed3
dysentry30 <bagassatrianurwinanto@gmail.com>
2021-11-04 00:24:21 +0700

    Creating Dashboard Page

--- public/js/login.js
+++ public/js/login.js
@@ -7,20 +7,20 @@ const form = document.querySelector("form");
 
 inputSeePassword.addEventListener("change", e => {
     const checked = e.target.checked;
-    if(checked) {
+    if (checked) {
         inputPassword.setAttribute("type", "text");
     } else {
         inputPassword.setAttribute("type", "password");
     }
 });
 
-form.addEventListener("submit", e => {
+form.addEventListener("submit", async e => {
     e.preventDefault();
     const formData = new FormData(form);
     const username = formData.get("username").toString();
     const password = formData.get("password").toString();
-    
-    if(!username) {
+
+    if (!username) {
         inputUsername.classList.add("border-danger");
         errorUsernameElt.classList.remove("hide");
         errorUsernameElt.querySelector("small").textContent = "Fill up this field above";
@@ -30,7 +30,7 @@ form.addEventListener("submit", e => {
         errorPasswordElt.classList.remove("hide");
         errorPasswordElt.querySelector("small").textContent = "Fill up this field above";
         return;
-    }else {
+    } else {
         inputUsername.classList.remove("border-danger");
         errorUsernameElt.classList.add("hide");
         errorUsernameElt.querySelector("small").textContent = "";
@@ -43,6 +43,50 @@ form.addEventListener("submit", e => {
         password
     }
 
+    const result = await fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data),
+    }).then(resolve => resolve.json());
+    if (result.status !== "Failed") {
+        return location.href = "/";
+    }
+    Swal.fire({
+        icon: "error",
+        title: result.status,
+        text: result.message,
+    });
     // ! connect to database
 
-})
+});
+
+// fetch(`/readMe.txt`)
+//     .then(resolve => resolve.body)
+//     .then(async body => {
+//         const reader = body.pipeThrough(new TextDecoderStream()).getReader();
+//         while(true) {
+//             const { value, done } = await reader.read();
+//             if (done) break;
+//             console.log("Received\n", value);
+//         }
+//         console.log("done");
+//         // return new ReadableStream({
+//         //     start(controller) {
+
+//         //         // const pump = () => {
+
+//         //         //     return pump();
+//         //         // }
+//         //     }
+//         // })
+//     })
+// .then(stream => new Response(stream))
+// .then(response => response.text())
+// .then(text => {
+//     const p = document.createElement("p");
+//     document.querySelector("#test").textContent = text;
+//     console.log("done");
+// })
+// .catch(err => console.log(err));
--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const sessionManager = require("session-manager").create({engine: "memory"});
+// const sessionManager = require("session-manager").create({engine: "memory"});
 const sql = require("mysql2");
+const sessionMysql = require("mysql2/promise");
 
 const server = express();
 const PORT = 8080;
 
+const cashierDB = sql.createConnection({
+    host: "localhost",
+    database: "cashier_system",
+    user: "root",
+    password: ""
+})
+
 const checkUserSession = (req, res, next) => {
     const session = sessionManager.start(req, res);
-    if(session.get("user")) {
+    if (session.get("user")) {
         return next()
+
     }
     return res.redirect("/login");
 }
@@ -27,6 +36,25 @@ server.get("/login", (req, res) => {
     res.sendFile(`${__dirname}/public/login.html`);
 });
 
-server.get("/", checkUserSession, (req, res) => {
-    res.sendFile(`${__dirname}/public/index.html`);
+server.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/public/home.html`);
 });
+
+server.post("/login", (req, res) => {
+    const {
+        username,
+        password
+    } = req.body;
+    const query = `SELECT * FROM users WHERE username="${username.toString().trim()}" AND username="${password.toString().trim()}"`
+    cashierDB.query(query, (err, rows) => {
+        if(err) throw err;
+        if(rows[0]) {
+            res.json(rows[0]);
+        } else {
+            res.json({
+                status: "Failed",
+                message: "User not found"
+            });
+        }
+    })
+})
